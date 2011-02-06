4d0290073db41692ad5b6910a2904432bd14bb87
oliver.ferrigni <oliver.ferrigni@asolutions.com>
2011-02-06 04:12:39 -0600

    Next step will look through located new branches and find those marked for code review.
    Oliver

--- shtests/gitBan.sh
+++ shtests/gitBan.sh
@@ -9,3 +9,19 @@ findNewBranches() {
 	echo "No.New.Branches"
 	fi
 }
+
+nextStep() {
+	stuff=( "$@" )
+	code_reviews=''
+	counter=0
+	for i in "${stuff[@]}";
+		do
+		has_code_review=`echo "$i" | grep cr`
+		if [[ -n "$has_code_review" ]]
+		then
+			code_reviews=$has_code_review
+		fi;
+	done;
+	echo $code_reviews		
+}
+
